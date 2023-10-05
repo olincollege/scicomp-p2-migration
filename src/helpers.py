8040b6a3b751caa514f155237d9b0ce0dc6af38e
KLA-pie <klieatjam@olin.edu>
2023-10-05 04:20:37 -0400

    Set simulation parameter constants

---
+++ src/helpers.py
@@ -0,0 +1,13 @@
+"""
+Initial and Nonvariable Condition setting and PDF allocation
+"""
+# Note: All units have been converted into SI Units
+BOLTZMANN_CONSTANT = 1.38 * 10 ** (-23)  # 1.38 * 10^-23 J/K
+RAD_MERCURY = 2.439 * 10**6  # 2,439 km
+GRAV_MERCURY = 3.705  # 3.705 m/s^2
+ESC_MERCURY = 4.251 * 10**3  # 4.251 km/s
+SURFACE_TEMPERATURE = 1.381 * 10**2  # 138.1 K
+TERMINATOR_MERCURY = 3.785 * 10**2  # 378.5 K
+N = 3.7 * 10 ** (-1)  # Run Parameter
+WATER_MASS = 2.989 * 10 ** (-26)  # 18.02 amu
+CARBON_DIOXIDE_MASS = 7.308 * 10 ** (-26)  # 44.01 amu
