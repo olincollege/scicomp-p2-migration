64dd5336e90e791912895789111caf624c967026
KLA-pie <klieatjam@olin.edu>
2023-10-05 23:55:09 -0400

    Set volatile characteristics

--- src/agents.py
+++ src/agents.py
@@ -1,3 +1,17 @@
 """
-Defines an agent utilized in the model
+Defines a volatile agent utilized in the model
 """
+
+
+class Volatile:
+
+    """
+    Define characteristics for a volatile molecule that migrates
+    along the surface of Mercury
+    """
+
+    def __init__(self) -> None:
+        self.temperature = 0
+        self.theta = 0
+        self.phi = 0
+        self.emergent_angle = 0
