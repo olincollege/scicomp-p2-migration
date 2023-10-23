12b56c6230d731b25d7cc77a1769567f6a40d61d
KLA-pie <klieatjam@olin.edu>
2023-10-23 00:34:24 -0400

    First visualization iteration

--- src/model.py
+++ src/model.py
@@ -1,3 +1,11 @@
 """
 Visualization sets for the simulation
 """
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_histogram(data_list, bins=10):
+    data = np.array(data_list)
+    data = data / 1000
+    return plt.hist(data, bins=bins)
