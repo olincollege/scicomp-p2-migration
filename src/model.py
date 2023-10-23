eb395253b7349ca70189895452b558b1c9687d28
KLA-pie <klieatjam@olin.edu>
2023-10-23 01:52:34 -0400

    Added Stacked Bar Graph

--- src/model.py
+++ src/model.py
@@ -29,3 +29,12 @@ def bar_plot(jeans_data, cold_data, photo_data):
     bar_labels = ["Jeans Escape", "Cold Trap", "Photodestruction"]
     bar_colors = ["tab:red", "tab:blue", "tab:green"]
     return plt.bar(bar_labels, bars, color=bar_colors)
+
+
+def stacked_bar_plot(jeans_data, cold_data, photo_data):
+    jeans_data = np.array(jeans_data) / 1000
+    cold_data = np.array(cold_data) / 1000
+    photo_data = np.array(photo_data) / 1000
+    plt.bar("Percentages", photo_data, color="green")
+    plt.bar("Percentages", cold_data, bottom=photo_data, color="blue")
+    return plt.bar("Percentages", jeans_data, bottom=cold_data, color="red")
