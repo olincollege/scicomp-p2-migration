4d2fd9a326cf68b169895fdd0a974143fe217f1c
KLA-pie <klieatjam@olin.edu>
2023-10-23 00:56:32 -0400

    Change histogram features

--- src/model.py
+++ src/model.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_histogram(data_list, bins=10):
+def plot_histogram(data_list, color, bins=10):
     data = np.array(data_list)
     data = data / 1000
-    return plt.hist(data, bins=bins)
+    return plt.hist(data, bins=bins, color=color)
